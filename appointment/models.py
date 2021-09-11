ca16a1db42317aed86e0d284d537f6220ede699d
radhika <aairamahajan@gmail.com>
2021-09-10 22:03:19 -0400

    implemened chat box form

--- appointment/models.py
+++ appointment/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class CreateAppointment(models.Model):
+    id = models.AutoField(auto_created=True, primary_key=True)
+    first_name = models.CharField(max_length=255, unique=False, null=False)
+    last_name = models.CharField(max_length=255, unique=False, null=False)
--- appointment/views.py
+++ appointment/views.py
@@ -1,3 +1,24 @@
+from appointment.models import CreateAppointment
+from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+
+
+# @csrf_exempt
+def create_new_appointment(request):
+
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        # Saving to the database
+        new_appointment = CreateAppointment(1, first_name, last_name)
+        new_appointment.save()
+
+    context = {}
+    return render(request, 'create_appointment.html', context)
+
+
+def chatbot_index(request):
+    return render(request, 'chatbot_index.html', {})
