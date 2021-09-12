71ff4189e39e3012c7f841ac23af0e00e6c8cf68
radhika <aairamahajan@gmail.com>
2021-09-12 19:52:39 -0400

    added model CreateAppointment & CreateFutureAppointment

--- appointment/admin.py
+++ appointment/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.db import models
 from .models import CreateAppointment, CreateFutureAppointment
 
 admin.site.register(CreateAppointment)
@@ -8,8 +9,14 @@ admin.site.register(CreateFutureAppointment)
 
 
 class CreateAppointmentAdmin(admin.ModelAdmin):
+
     list_display = ("id", "first_name", "last_name", "phone",
                     "email", "city", "state", "country")
+
+    first_name = models.CharField(max_length=50)
+
+    def first_name(self):
+        return self.first_name
 
 
 class CreateFutureAppointmentAdmin(admin.ModelAdmin):
